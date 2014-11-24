1bf25250e1d25db821e81fd2af0f12c56fa63931
Ricky Ing <ricky@2600hz.com>
2014-11-23 19:16:41 -0800

    create callflow function

--- examples/examples.php
+++ examples/examples.php
@@ -4,20 +4,22 @@ use \Kazoo\AuthToken\User;
 use \Kazoo\SDK;
 
 class HostileWorkEnvironment(){
-        private static $account_id = "gjaflkjsljfa;sldkjfs"; 
-        private static $sdk;
-      
-        private function getSdk(){
-            $options = array('base_url' => 'http://192.168.56.101:8000');
-            $auth_token = new User('admin', 'admin', 'sip.ing-local-dev.2600hz.com');
-            $sdk = new SDK($auth_token, $options);
-        }       
- 
-        private function getAccountId(){
-           return $this->account_id;
-        }
+
+    private static $account_id = "gjaflkjsljfa;sldkjfs"; 
+    private static $sdk;
+
+    private function getSdkInstance(){
+        $options = array('base_url' => 'http://192.168.56.101:8000');
+        $auth_token = new User('admin', 'admin', 'sip.ing-local-dev.2600hz.com');
+        $sdk = new SDK($auth_token, $options);
+    }       
+
+    private function getAccountId(){
+       return $this->account_id;
+    }
 
 	private function getAccount(){
+		$sdk = getSdkInstance();
 	    return $sdk->Account($this->getAccountId()); 
 	}
 
@@ -25,7 +27,7 @@ class HostileWorkEnvironment(){
 	    $user_id = createUser($username, $first_name, $last_name, $email);
 	    assignDevice($ext, $user_id);
 	    $vmbox_id = createVmbox($user_id);
-	    createCallflow($user_id, $vmbox);
+	    createCallflow($ext, $device_id, $vmbox_id);
 	}
 
 	public function fire($ext){
@@ -39,35 +41,51 @@ class HostileWorkEnvironment(){
                         
 	}
  
-	private function deleteUser(){
+	private function deleteUser($user_id){
             
 	}
 
-	private function deleteVmbox(){
+	private function deleteVmbox($vmbox){
+
 
 	}
 
-	private function createCallflow(){
+	private function createCallflow($ext, $device_id, $vmbox_id){
 
+	    $callflow = $this->getAccount()->callflow();
+	    $callflow->numbers = array($ext);
+	    $flow = new stdClass();
+
+	    $flow->module = "device";
+	    $flow->data->id = $device_id;
+	    $flow->data->timeout = "30";
+	    $flow->data->can_call_self = false;
+	    $flow->children->_->data->id = $vmbox_id;
+	    $flow->children->_->module = "voicemail";
+	    $flow->children->_->children = new stdClass();
+
+	    $callflow->flow = $flow;
+	    $callflow->save();
 	}
 
 	private function deleteCallflow(){
 
 	}
 
-	private function setRestriction($sdk, $restriction){
-             
+	private function setRestriction($device_id, $restrictions){
+    	$device = $this->getAccount->device($device_id);
 
+    	$device->save()
 	}
 
 	private function assignDevice($device_id, $owner_id, $ext){
-	    $device = $this->account->vmbox($vmbox_id);
+	    $device = $this->getAccount->vmbox($vmbox_id);
 	    $device->owner_id = $owner_id;
 	    $device->save();
 	}
 
 	private function assignVmbox($vmbox_id, $owner_id){
-	    $vmbox = $this->account->vmbox($vmbox_id);
+	    $vmbox = $this->getAccount->vmbox($vmbox_id);
 	    $vmbox->owner_id = $owner_id;
 	    $vmbox->save();
 	}
@@ -82,11 +100,12 @@ class HostileWorkEnvironment(){
 	    return $vmbox->getId();
 	}
 
-	private function createUser($sdk, $account_id, $user_name, $first_name, $last_name){
+	private function createUser($sdk, $account_id, $user_name, $first_name, $last_name, $email){
 	    $user = $this->getAccount()->vmbox();
 	    $user->username = $user_name;
 	    $user->first_name = $first_name;
 	    $user->last_name = $last_name;
+	    $user->email = $email;
 	    $user->save();
 
 	    return $user->getId();
@@ -101,7 +120,7 @@ class HostileWorkEnvironment(){
 	}
 
 	private function createAccount($sdk, $account_name) {
-            $account = $this->getAccount();
+        $account = $this->getAccount();
 	    $account = $sdk->Account(null);
 	    $account->name = $account_name;
 	    $account->save();
